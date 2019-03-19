532aae0bb620c2b4a2c871dbff0703459405c15b
Steven B. Oladipo <stevenboladipo@gmail.com>
2019-03-19 12:50:05 -0400

    added all files

---
+++ example.js
@@ -0,0 +1,14 @@
+// a function to show date 
+function showdate() {
+    document.getElementById('date').innerHTML=Date()
+}
+// a function to add two numbers 
+function add() {
+    var a = 10;
+    var b = 20;
+    // adds two numbers 
+    result = a + b;
+    document.getElementById("result").innerHTML = result;
+} 
+
+
